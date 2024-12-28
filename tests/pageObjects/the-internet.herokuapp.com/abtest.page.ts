18f63b0466cb827f8d63065770bbc1d634905759
Nguyen Phat <phatnt.uit@gmail.com>
2024-12-28 16:10:33 +0700

    test(e2e): add abtest page test cases

---
+++ tests/pageObjects/the-internet.herokuapp.com/abtest.page.ts
@@ -0,0 +1,55 @@
+import { $, browser } from '@wdio/globals';
+import Page from './page';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class ABTestPage extends Page {
+  // Reference: https://the-internet.herokuapp.com/js/vendor/298279967.js
+  private VARIATION_COOKIE_NAME = 'optimizelyBuckets';
+  private OPT_OUT_COOKIE_NAME = 'optimizelyOptOut';
+  private EXPERIMENT_ID = '298349752';
+  public VARIATIONS = {
+    Original: '298291000',
+    'Variation #1': '298343790',
+  };
+  /**
+   * define selectors using getter methods
+   */
+
+  public get header() {
+    return $('//*[@id="content"]/div/h3');
+  }
+
+  /**
+   * overwrite specific options to adapt it to page object
+   */
+  public async open() {
+    return super.open('abtest');
+  }
+
+  public async useVariation(variationId: string) {
+    await browser.deleteCookie(this.VARIATION_COOKIE_NAME);
+    await browser.setCookies({
+      name: this.VARIATION_COOKIE_NAME,
+      value: encodeURIComponent(
+        JSON.stringify({
+          [this.EXPERIMENT_ID]: variationId,
+        })
+      ),
+    });
+
+    await browser.refresh();
+  }
+
+  public async useOptOutCookie() {
+    await browser.setCookies({
+      name: this.OPT_OUT_COOKIE_NAME,
+      value: 'true',
+    });
+
+    await browser.refresh();
+  }
+}
+
+export default new ABTestPage();
---
+++ tests/specs/the-internet.herokuapp.com/abtest.e2e.ts
@@ -0,0 +1,35 @@
+import { expect } from '@wdio/globals';
+import abtestPage from 'tests/pageObjects/the-internet.herokuapp.com/abtest.page';
+
+describe('ABTest', () => {
+  // it is related to Cookies
+  // A/B Test Control
+  // A/B Test Variation 1
+  // No A/B Test
+
+  afterEach(async () => {
+    await browser.deleteAllCookies();
+  });
+
+  it(`should display page heading "A/B Test Control"`, async () => {
+    await abtestPage.open();
+    await abtestPage.useVariation(abtestPage.VARIATIONS.Original);
+
+    await expect(abtestPage.header).toHaveText('A/B Test Control');
+  });
+
+  it(`should display page heading "A/B Test Variation 1"`, async () => {
+    await abtestPage.open();
+    await abtestPage.useVariation(abtestPage.VARIATIONS['Variation #1']);
+
+    await expect(abtestPage.header).toHaveText('A/B Test Variation 1');
+  });
+
+  it('should display page heading "No A/B Test" when an opt-out cookie is set', async () => {
+    await abtestPage.open();
+    // Create an opt-out cookie in order to navigate to the No A/B Test Page
+    await abtestPage.useOptOutCookie();
+
+    await expect(abtestPage.header).toHaveText('No A/B Test');
+  });
+});
